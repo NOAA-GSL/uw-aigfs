df7a0edd8080c64cad8e073e7b464f0b7979c654
RT AIGFS <role.rtaigfs@ursa.fairmont.rdhpcs.noaa.gov>
2026-08-07 23:26:44 +0000

    Format shell code

--- scripts/run_post.sh
+++ scripts/run_post.sh
@@ -9,8 +9,9 @@ for prod in pres sfc; do
   file=aigfs.${cycle}.${prod}.f${FH}.grib2
 
   echo "[$(date)]: Opening GRIB2 file"
-  wgrib2 -s ${INPUTDIR}/${file} > ${INPUTDIR}/${file}.idx
-  export err=$?; err_chk
+  wgrib2 -s ${INPUTDIR}/${file} >${INPUTDIR}/${file}.idx
+  export err=$?
+  err_chk
   echo "[$(date)]: Index file created successfully"
 
 done
