6eb465e2f81429a54f5b6ce2816f61f61394ae7c
RT AIGFS <role.rtaigfs@ursa.fairmont.rdhpcs.noaa.gov>
2026-08-07 23:52:58 +0000

    Work on shfmt

--- scripts/run_post.sh
+++ scripts/run_post.sh
@@ -7,14 +7,12 @@ echo "Starting $0"
 cycle=${ISOCYCLE:11:2}
 
 for prod in pres sfc; do
-  file=aigfs.${cycle}.${prod}.f${FH}.grib2
-
+  file=aigfs.$cycle.$prod.f$FH.grib2
   echo "[$(date)]: Opening GRIB2 file"
-  wgrib2 -s ${INPUTDIR}/${file} >${INPUTDIR}/${file}.idx
+  wgrib2 -s $INPUTDIR/$file >$INPUTDIR/$file.idx
   export err=$?
   err_chk
   echo "[$(date)]: Index file created successfully"
-
 done
 
 echo "$0 completed normally"
