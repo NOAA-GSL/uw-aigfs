4b809ffc2ee5ec918af08abab137961472927caf
Paul Madden <paul.madden@noaa.gov>
2026-08-11 21:27:45 +0000

    Cleanup

--- ush/validation.py
+++ ush/validation.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
-from datetime import datetime, timedelta  # noqa: TC003
-from pathlib import Path  # noqa: TC003
+from typing import TYPE_CHECKING
 
 from pydantic import BaseModel, model_validator
+
+if TYPE_CHECKING:
+    from datetime import datetime, timedelta
+    from pathlib import Path
 
 
 class Config(BaseModel):
