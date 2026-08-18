90baa2d81175ce25519821a5b3a1b3cd78236dd6
Paul Madden <paul.madden@noaa.gov>
2026-08-18 16:03:18 +0000

    Simplify

--- lib/aigfs/validation.py
+++ lib/aigfs/validation.py
@@ -2,10 +2,8 @@
 Support for validating AIGFS configurations.
 """
 
-from __future__ import annotations
-
-from datetime import datetime, timedelta  # noqa: TC003
-from pathlib import Path  # noqa: TC003
+from datetime import datetime, timedelta
+from pathlib import Path
 
 from pydantic import BaseModel, model_validator
 
@@ -22,7 +20,7 @@ class App(BaseModel):
     rundir: Path
 
     @model_validator(mode="after")
-    def first_and_last_cycle(self) -> App:
+    def first_and_last_cycle(self) -> "App":
         if self.last_cycle < self.first_cycle:
             msg = "last_cycle cannot precede first_cycle"
             raise ValueError(msg)
